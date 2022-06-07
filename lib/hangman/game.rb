c66a368f62cf87fa3a3571de77404ceb12c79e25
amyspencerproject <amy@spencerproject.com>
2022-06-07 16:23:54 -0600

    Paritally fixed bug for empty guess and fixed bug repeated wrong guess.

--- lib/hangman/game.rb
+++ lib/hangman/game.rb
@@ -1,11 +1,12 @@
 module Hangman
   class Game
-    attr_reader :chances, :word, :wrong_tries, :guess
+    attr_reader :chances, :word, :wrong_tries, :guess, :wrong_guess
 
     def initialize
       @chances = 5
       @wrong_tries = 0
       @guess = ""
+      @wrong_guess = ""
       @word = Dictionary.random
     end
 
@@ -13,13 +14,31 @@ module Hangman
       Graphics.clear_screen
       puts 'Guess this word: ' + Graphics.obfuscate_word(word, '')
 
+      # char = gets.chomp
+      char = gets.strip
+      empty = char.empty?
+      # puts char.class
+      # puts empty
+      if empty = true
+          puts "You must enter a letter to play"
+          puts 'Try again: ' + Graphics.obfuscate_word(word, guess)
+      end
+
       while true
         print "[#{chances - wrong_tries} chances left]: "
 
-        char = gets.chomp
+        char = gets.strip
         Graphics.clear_screen
+        
+        # empty = char.empty?
+        
+        # if empty = true
+        #   puts "You must enter a letter to play"
+        #   puts 'Try again: ' + Graphics.obfuscate_word(word, guess)
+        # end
 
         if word.include? char
+        
           if guess.include? char
             puts "You already entered '#{char}'. Yes, it is still correct.. 🙄"
             puts 'Try again: ' + Graphics.obfuscate_word(word, guess)
@@ -36,8 +55,15 @@ module Hangman
             break
           end
         else
-          puts "OH NOES! The word doesn't contain '#{char}'"
-          @wrong_tries = @wrong_tries + 1
+          if wrong_guess.include? char
+            
+            puts "You already entered that and the word still doesn't contain '#{char}' "
+          else 
+            wrong_guess << char
+            puts "OH NOES! The word doesn't contain '#{char}'"
+            @wrong_tries = @wrong_tries + 1
+          end
+          
 
           if wrong_tries == chances
             puts Graphics::DEAD
