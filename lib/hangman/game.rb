6ea922626e97393fca7b34a6dcee461059e04e91
amyspencerproject <amy@spencerproject.com>
2022-06-07 19:34:43 -0600

    Added if statement that only accepts letters to play game

--- lib/hangman/game.rb
+++ lib/hangman/game.rb
@@ -13,31 +13,18 @@ module Hangman
     def play
       Graphics.clear_screen
       puts 'Guess this word: ' + Graphics.obfuscate_word(word, '')
-
-      # char = gets.chomp
-      char = gets.strip
-      empty = char.empty?
-      # puts char.class
-      # puts empty
-      if empty = true
-          puts "You must enter a letter to play"
-          puts 'Try again: ' + Graphics.obfuscate_word(word, guess)
-      end
-
+      
       while true
         print "[#{chances - wrong_tries} chances left]: "
 
         char = gets.strip
         Graphics.clear_screen
         
-        # empty = char.empty?
-        
-        # if empty = true
-        #   puts "You must enter a letter to play"
-        #   puts 'Try again: ' + Graphics.obfuscate_word(word, guess)
-        # end
+        if /[[:alpha:]]/.match(char) == nil
+          puts "You must enter a letter to play"
+          puts 'Try again: ' + Graphics.obfuscate_word(word, guess)
 
-        if word.include? char
+        elsif word.include? char
         
           if guess.include? char
             puts "You already entered '#{char}'. Yes, it is still correct.. 🙄"
@@ -56,14 +43,12 @@ module Hangman
           end
         else
           if wrong_guess.include? char
-            
             puts "You already entered that and the word still doesn't contain '#{char}' "
           else 
             wrong_guess << char
             puts "OH NOES! The word doesn't contain '#{char}'"
             @wrong_tries = @wrong_tries + 1
           end
-          
 
           if wrong_tries == chances
             puts Graphics::DEAD
